23589812ef502debf0be140f4673c32b606ba7aa
Bettina Lanser <bettina.lanser@codecentric.de>
2025-04-25 13:47:21 +0200

    turn fuel type filters into radio group

--- src/components/fuel-type-filter.tsx
+++ src/components/fuel-type-filter.tsx
@@ -11,20 +11,25 @@ const FuelTypeFilter = ({
 }) => {
   return (
     <>
-      <div className="flex flex-col gap-4 px-4">
-        <div className="text-xl">Fuel Type</div>
+      <fieldset className="flex flex-col gap-4 px-4">
+        <legend className="text-xl">Fuel Type</legend>
         <div className="flex flex-col">
           {fuelTypeFilterValues.map((filterValue) => (
-            <button
+            <>
+            <input
+              type="radio"
+              name="fuelType"
               onClick={() => filterByFuelType(filterValue)}
               key={filterValue}
+              value={filterValue}
+              id={filterValue}
               className={`cursor-pointer border border-solid border-gray-200 px-6 py-2 ${filterValue === currentFilter ? "bg-gray-200" : ""}`}
-            >
-              {filterValue}
-            </button>
+            />
+            <label htmlFor={filterValue}>{filterValue}</label>
+            </>
           ))}
         </div>
-      </div>
+      </fieldset>
     </>
   );
 };
