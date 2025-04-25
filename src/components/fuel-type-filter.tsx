f1a47e2152045a98964bd547114a520c4716e8e6
Bettina Lanser <bettina.lanser@codecentric.de>
2025-04-25 14:17:19 +0200

    a11y: make whole label of radio group element clickable

--- src/components/fuel-type-filter.tsx
+++ src/components/fuel-type-filter.tsx
@@ -15,20 +15,21 @@ const FuelTypeFilter = ({
         <legend className="text-xl">Fuel Type</legend>
         <div className="flex flex-col">
           {fuelTypeFilterValues.map((filterValue) => (
-            <p
-              className={`cursor-pointer border border-solid border-gray-200 px-6 py-2 ${filterValue === currentFilter ? "bg-gray-200" : ""}`}
-              key={filterValue}>
-            <input
-              className="appearance-none"
-              type="radio"
-              checked={filterValue === currentFilter}
-              name="fuelType"
-              onClick={() => filterByFuelType(filterValue)}
-              value={filterValue}
-              id={filterValue}
-            />
-            <label htmlFor={filterValue}>{filterValue}</label>
-            </p>
+            <label
+              key={filterValue}
+              htmlFor={filterValue}
+              className={`cursor-pointer border border-solid border-gray-200 px-6 py-2 ${filterValue === currentFilter ? "bg-gray-200" : ""}`}>
+              <input
+                className="appearance-none"
+                type="radio"
+                checked={filterValue === currentFilter}
+                name="fuelType"
+                onClick={() => filterByFuelType(filterValue)}
+                value={filterValue}
+                id={filterValue}
+              />
+              {filterValue}
+            </label>
           ))}
         </div>
       </fieldset>
